27def1600da902de23617565f3101f0f6d708dd4
Mohammad Imran <imrancrazy200@gmail.com>
2022-03-26 22:43:19 +0530

    feat: add loading skeleton

--- components/EventList/index.js
+++ components/EventList/index.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import {
   Table,
   Thead,
@@ -10,6 +9,7 @@ import {
   Icon,
   Image,
   Button,
+  Skeleton,
 } from "@chakra-ui/react";
 import { CogIcon } from "@heroicons/react/solid";
 
@@ -19,58 +19,59 @@ import useFetchEvents from "./useFetchEvents";
 const EventList = () => {
   const { isLoading, isError, events, error } = useFetchEvents();
 
-  if (isLoading) {
-    return <span>Loading...</span>;
-  }
-
   if (isError) {
     return <span>Error: {error.message}</span>;
   }
 
   return (
-    <Table variant="simple">
-      <TableCaption>Events (All)</TableCaption>
-      <Thead>
-        <Tr>
-          <Th>Preview Image</Th>
-          <Th>Title</Th>
-          <Th>Date</Th>
-          <Th>Start Time</Th>
-          <Th>People</Th>
-          <Th>Manage</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {events.map((event) => (
-          <Tr key={event._id} cursor="pointer">
-            <Td>
-              <Image
-                src={event?.coverImageUrl}
-                alt="preview-image"
-                objectFit="cover"
-                borderRadius="5px"
-                w="150px"
-                h="100px"
-              />
-            </Td>
-            <Td>{event?.title}</Td>
-            <Td>{getLocalizedDate(event?.eventDate)}</Td>
-            <Td>{getLocalizedTime(event?.eventDate)}</Td>
-            <Td>0</Td>
-            <Td>
-              <Button
-                variant={"outline"}
-                colorScheme={"gray"}
-                size={"sm"}
-                leftIcon={<Icon as={CogIcon} w={5} h={5} color="gray.600" />}
-              >
-                Settings
-              </Button>
-            </Td>
+    <Skeleton isLoaded={!isLoading}>
+      <Table variant="simple">
+        <TableCaption>Events (All)</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Preview Image</Th>
+            <Th>Title</Th>
+            <Th>Date</Th>
+            <Th>Start Time</Th>
+            <Th>People</Th>
+            <Th>Manage</Th>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {events &&
+            events.map((event) => (
+              <Tr key={event._id} cursor="pointer">
+                <Td>
+                  <Image
+                    src={event?.coverImageUrl}
+                    alt="preview-image"
+                    objectFit="cover"
+                    borderRadius="5px"
+                    w="150px"
+                    h="100px"
+                  />
+                </Td>
+                <Td>{event?.title}</Td>
+                <Td>{getLocalizedDate(event?.eventDate)}</Td>
+                <Td>{getLocalizedTime(event?.eventDate)}</Td>
+                <Td>0</Td>
+                <Td>
+                  <Button
+                    variant={"outline"}
+                    colorScheme={"gray"}
+                    size={"sm"}
+                    leftIcon={
+                      <Icon as={CogIcon} w={5} h={5} color="gray.600" />
+                    }
+                  >
+                    Settings
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
+        </Tbody>
+      </Table>
+    </Skeleton>
   );
 };
 
