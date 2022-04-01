ea8b295d46d012fa3a065c3d07eb96d3cf0181ab
Mohammad Imran <imrancrazy200@gmail.com>
2022-04-01 23:13:57 +0530

    feat: add utility function to protect from unauthorized access

---
+++ core/utils/authorized.js
@@ -0,0 +1,20 @@
+import dbConnect from "core/db/connect";
+import Event from "core/db/models/Event";
+
+// Input: event_id: String, user_id: String
+// Output: event: Object || null
+export default async function isAuthorized(event_id, user_id) {
+  await dbConnect();
+
+  const event = await Event.findById(event_id).lean();
+  if (!event) {
+    return null;
+  }
+
+  const isOwner = !!event.hosts.find((hostId) => hostId.valueOf() === user_id);
+  if (!isOwner) {
+    return null;
+  }
+
+  return event;
+}
