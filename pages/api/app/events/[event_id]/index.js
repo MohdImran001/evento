c8af2409796fb7f9906cdf437ef389b743938b8b
Mohammad Imran <imrancrazy200@gmail.com>
2022-04-01 23:15:23 +0530

    feat: add api to retrive event information

---
+++ pages/api/app/events/[event_id]/index.js
@@ -0,0 +1,32 @@
+import { getToken } from "next-auth/jwt";
+import isAuthorized from "core/utils/authorized";
+
+const secret = process.env.NEXTAUTH_SECRET;
+
+export default async function handler(req, res) {
+  // Protect from unauthorized access
+  const { event_id } = req.query;
+  const { sub: user_id } = await getToken({ req, secret });
+  const event = await isAuthorized(event_id, user_id);
+  if (!event) {
+    return res.status(403).json({ message: "Not Authorized" });
+  }
+
+  // Return only desired fields
+  const fields = req.query.fields?.split(",");
+  if (fields && fields.length > 0) {
+    const filteredEventData = Object.keys(event)
+      .filter((key) => fields.includes(key))
+      .reduce((obj, key) => {
+        return {
+          ...obj,
+          [key]: event[key],
+        };
+      }, {});
+
+    return res.status(200).json({ ...filteredEventData });
+  }
+
+  // Return complete event object
+  return res.status(200).json({ ...event });
+}
