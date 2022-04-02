14a1832e0aa1dfe1a59f2b252c251f48d7999785
Mohammad Imran <imrancrazy200@gmail.com>
2022-04-02 17:54:45 +0530

    feat: add user name on profile badge

--- core/elements/User/Profile.jsx
+++ core/elements/User/Profile.jsx
@@ -1,9 +1,16 @@
 import { useSession } from "next-auth/react";
-import { Avatar } from "@chakra-ui/react";
+import { Box, Avatar, Text, Flex } from "@chakra-ui/react";
 
 export default function Profile() {
   const { data } = useSession();
   if (!data) return <Avatar size={"sm"} />;
 
-  return <Avatar size={"sm"} src={data?.user?.image} />;
+  return (
+    <Box> 
+      <Flex>
+        <Text mt="7px" px="10px" fontWeight="normal">{data?.user?.name}</Text>
+        <Avatar  size={"sm"} src={data?.user?.image} />
+      </Flex>
+    </Box>
+  ) 
 }
