4551c37bc7a3468b424d42852fcf731899afa960
Mohammad Imran <imrancrazy200@gmail.com>
2022-04-04 11:48:22 +0530

    feat: custom hook to fetch event by id

---
+++ components/EventSettings/useEvent.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchEventById = (event_id, fields) => {
+  let url = `/api/app/events/${event_id}`;
+  if (fields.length > 0) url += `?fields=${fields.split(" ").join(",")}`;
+
+  return axios.get(url);
+};
+
+/**
+ *
+ * @param {string} event_id - id of the event
+ * @param {string} fields   - a string of space seperated values to be fetch from event api
+ * @returns {object}        - event object with the required fields
+ */
+export default function useEvent(event_id, fields = "") {
+  const { isLoading, isError, isSuccess, data, error } = useQuery(
+    "fetchEventById",
+    () => fetchEventById(event_id, fields)
+  );
+
+  return {
+    isLoading,
+    isError,
+    event: isSuccess ? data.data : null,
+    error,
+  };
+}
