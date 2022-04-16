75967c9ea8785b863d64ac4b9b7b4abf74a93248
Mohammad Imran <imrancrazy200@gmail.com>
2022-04-16 14:17:17 +0530

    fix: add proper naming to useQuery hooks

--- pages/api/app/events/[event_id]/edit.js
+++ pages/api/app/events/[event_id]/edit.js
@@ -3,19 +3,27 @@ import isAuthorized from "core/utils/authorized";
 
 const secret = process.env.NEXTAUTH_SECRET;
 
-// PUT: Update event data
-// TODO: Add JS_DOC
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ * @returns - success/error
+ */
 export default async function handler(req, res) {
   // Only HTTP: PUT method is allowed to update data
   if (req.method !== "PUT") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 
-  // Protect from unauthorized access
   const { event_id } = req.query;
+  const { eventData } = req.body;
   const { sub: user_id } = await getToken({ req, secret });
+
+  // Protect from unauthorized access
   const event = await isAuthorized(event_id, user_id);
   if (!event) {
     return res.status(403).json({ message: "Not Authorized" });
   }
+
+  res.status(200).json({ ...eventData });
 }
