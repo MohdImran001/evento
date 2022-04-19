a347d43f90fdd030ea7817d0ac11b237f4589167
Mohammad Imran <imrancrazy200@gmail.com>
2022-04-19 23:59:08 +0530

    fix: add proper loader in new event button

--- components/EventList/index.jsx
+++ components/EventList/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
 
 import {
@@ -46,15 +47,16 @@ import useNewEvent from "lib/hooks/mutations/useNewEvent";
 
 const EventList = () => {
   const router = useRouter();
+  const { mutate } = useNewEvent();
+  const [loading, setLoading] = useState(false);
   const { isLoading, isError, events, error } = useEvents();
-  const newEvent = useNewEvent();
 
   if (isError) {
     return <span>Error: {error.message}</span>;
   }
 
   return (
-    <Skeleton isLoaded={!isLoading}>
+    <>
       <Box>
         <Heading size="4xl" m="5rem auto" color="brand">
           Events
@@ -68,121 +70,126 @@ const EventList = () => {
             color="#FFF"
             _hover={{ bg: "brandOrange" }}
             _active={{ bg: "brandOrange" }}
-            onClick={async () => (await newEvent).mutate()}
-            loading={newEvent.isLoading}
+            onClick={() => {
+              setLoading(true);
+              mutate();
+            }}
+            isLoading={loading}
           >
             New Event
           </Button>
         </Box>
       </Box>
-      <Table variant="simple">
-        <TableCaption>Events (All)</TableCaption>
-        <Thead bg="orange.100">
-          <Tr>
-            <Th>Event</Th>
-            <Th>People</Th>
-            <Th>Status</Th>
-            <Th></Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {events &&
-            events.map((event) => (
-              <Tr
-                key={event._id}
-                color="secondary"
-                fontSize="sm"
-                _hover={{ shadow: "base" }}
-              >
-                <Td>
-                  <Flex experimental_spaceX="20px">
-                    <Box bg="white" w="70px" borderRadius="5px">
-                      <Center w="100%" p="2px">
-                        <Text fontSize={{ base: "xs" }} color="red.500">
-                          <b>{getMonth(event?.eventDate).toUpperCase()}</b>
-                        </Text>
-                      </Center>
-                      <Center mt="6px">
-                        <Heading size="lg" color="secondary">
-                          {getDate(event?.eventDate)}
+      <Skeleton isLoaded={!isLoading}>
+        <Table variant="simple">
+          <TableCaption>Events (All)</TableCaption>
+          <Thead bg="orange.100">
+            <Tr>
+              <Th>Event</Th>
+              <Th>People</Th>
+              <Th>Status</Th>
+              <Th></Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {events &&
+              events.map((event) => (
+                <Tr
+                  key={event._id}
+                  color="secondary"
+                  fontSize="sm"
+                  _hover={{ shadow: "base" }}
+                >
+                  <Td>
+                    <Flex experimental_spaceX="20px">
+                      <Box bg="white" w="70px" borderRadius="5px">
+                        <Center w="100%" p="2px">
+                          <Text fontSize={{ base: "xs" }} color="red.500">
+                            <b>{getMonth(event?.eventDate).toUpperCase()}</b>
+                          </Text>
+                        </Center>
+                        <Center mt="6px">
+                          <Heading size="lg" color="secondary">
+                            {getDate(event?.eventDate)}
+                          </Heading>
+                        </Center>
+                      </Box>
+                      <Image
+                        src={
+                          event.coverImageUrl.length > 0
+                            ? event.coverImageUrl
+                            : "/no-image.png"
+                        }
+                        alt="preview-image"
+                        objectFit="cover"
+                        borderRadius="2px"
+                        w="70px"
+                        h="70px"
+                      />
+                      <Box ml="1rem">
+                        <Heading size="md" fontWeight="normal" color="brand">
+                          {event?.title}
                         </Heading>
-                      </Center>
-                    </Box>
-                    <Image
-                      src={
-                        event.coverImageUrl.length > 0
-                          ? event.coverImageUrl
-                          : "/no-image.png"
-                      }
-                      alt="preview-image"
-                      objectFit="cover"
-                      borderRadius="2px"
-                      w="70px"
-                      h="70px"
-                    />
-                    <Box ml="1rem">
-                      <Heading size="md" fontWeight="normal" color="brand">
-                        {event?.title}
-                      </Heading>
-                      <Text mt="8px">{event?.location?.name}</Text>
-                      <Text>
-                        {getLocalizedDate(event?.eventDate)} at{" "}
-                        {getLocalizedTime(event?.eventStartTime)}
-                      </Text>
-                    </Box>
-                  </Flex>
-                </Td>
-                <Td>0</Td>
-                <Td>{event?.status}</Td>
-                <Td>
-                  <Menu>
-                    <MenuButton
-                      as={IconButton}
-                      aria-label="Options"
-                      icon={<Icon as={DotsVerticalIcon} w={5} />}
-                      variant="outline"
-                    />
-                    <MenuList>
-                      <MenuItem
-                        icon={<Icon as={ExternalLinkIcon} w={5} h={5} />}
-                        onClick={() =>
-                          window.open(
-                            "http://localhost:3000/" + event._id,
-                            "_blank"
-                          )
-                        }
-                      >
-                        View
-                      </MenuItem>
-                      <MenuItem
-                        icon={<Icon as={ClipboardCopyIcon} w={5} h={5} />}
-                        onClick={() =>
-                          copyUrl("http://localhost:3000/" + event._id)
-                        }
-                      >
-                        Copy URL
-                      </MenuItem>
-                      <MenuItem
-                        icon={<Icon as={PencilAltIcon} w={5} h={5} />}
-                        onClick={() =>
-                          router.push(`/app/events/${event._id}/edit`)
-                        }
-                      >
-                        Edit
-                      </MenuItem>
-                      <MenuItem
-                        icon={<Icon as={DocumentRemoveIcon} w={5} h={5} />}
-                      >
-                        Delete
-                      </MenuItem>
-                    </MenuList>
-                  </Menu>
-                </Td>
-              </Tr>
-            ))}
-        </Tbody>
-      </Table>
-    </Skeleton>
+                        <Text mt="8px">{event?.location?.name}</Text>
+                        <Text>
+                          {getLocalizedDate(event?.eventDate)} at{" "}
+                          {getLocalizedTime(event?.eventStartTime)}
+                        </Text>
+                      </Box>
+                    </Flex>
+                  </Td>
+                  <Td>0</Td>
+                  <Td>{event?.status}</Td>
+                  <Td>
+                    <Menu>
+                      <MenuButton
+                        as={IconButton}
+                        aria-label="Options"
+                        icon={<Icon as={DotsVerticalIcon} w={5} />}
+                        variant="outline"
+                      />
+                      <MenuList>
+                        <MenuItem
+                          icon={<Icon as={ExternalLinkIcon} w={5} h={5} />}
+                          onClick={() =>
+                            window.open(
+                              "http://localhost:3000/" + event._id,
+                              "_blank"
+                            )
+                          }
+                        >
+                          View
+                        </MenuItem>
+                        <MenuItem
+                          icon={<Icon as={ClipboardCopyIcon} w={5} h={5} />}
+                          onClick={() =>
+                            copyUrl("http://localhost:3000/" + event._id)
+                          }
+                        >
+                          Copy URL
+                        </MenuItem>
+                        <MenuItem
+                          icon={<Icon as={PencilAltIcon} w={5} h={5} />}
+                          onClick={() =>
+                            router.push(`/app/events/${event._id}/edit`)
+                          }
+                        >
+                          Edit
+                        </MenuItem>
+                        <MenuItem
+                          icon={<Icon as={DocumentRemoveIcon} w={5} h={5} />}
+                        >
+                          Delete
+                        </MenuItem>
+                      </MenuList>
+                    </Menu>
+                  </Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      </Skeleton>
+    </>
   );
 };
 
