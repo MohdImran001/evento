53b2061d5018616ed5b52e2edf6efdeb65c0bac0
Mohammad Imran <imrancrazy200@gmail.com>
2022-05-02 15:51:31 +0530

    fix: add skeleton to event heading

--- components/EventSettings/Header/index.jsx
+++ components/EventSettings/Header/index.jsx
@@ -1,4 +1,12 @@
-import { Box, Heading, Text, IconButton, Icon, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Text,
+  IconButton,
+  Icon,
+  Flex,
+  Skeleton,
+} from "@chakra-ui/react";
 import { DocumentDuplicateIcon } from "@heroicons/react/solid";
 
 import { BASE_URL } from "core/constants";
@@ -13,31 +21,32 @@ export default function Header({ event_id }) {
   );
 
   return (
-    <Box>
-      <Heading size="3xl" maxW="70rem" m="5rem auto" color="brand">
-        {isLoading && "loading..."}
-        {isError && error.message}
-        {event && event.title}
-      </Heading>
-      {event?.eventDate && (
-        <Text color="brandOrange" mt="-4rem">
-          {getLocalizedDate(event?.eventDate)} at{" "}
-          {getLocalizedTime(event?.eventStartTime)}
-        </Text>
-      )}
-      <Flex mt="1rem" experimental_spaceX={2}>
-        <Text
-          color="secondary"
-          border={"1px solid #eee"}
-          px="10px"
-          py="7px"
-          borderRadius="2px"
-          bg="gray.100"
-        >
-          {BASE_URL + `/${event?._id}`}
-        </Text>
-        <IconButton icon={<Icon as={DocumentDuplicateIcon} w={5} h={5} />} />
-      </Flex>
-    </Box>
+    <Skeleton isLoaded={!isLoading}>
+      <Box>
+        <Heading size="3xl" maxW="70rem" m="5rem auto" color="brand">
+          {isError && error.message}
+          {event && event.title}
+        </Heading>
+        {event?.eventDate && (
+          <Text color="brandOrange" mt="-4rem">
+            {getLocalizedDate(event?.eventDate)} at{" "}
+            {getLocalizedTime(event?.eventStartTime)}
+          </Text>
+        )}
+        <Flex mt="1rem" experimental_spaceX={2}>
+          <Text
+            color="secondary"
+            border={"1px solid #eee"}
+            px="10px"
+            py="7px"
+            borderRadius="2px"
+            bg="gray.100"
+          >
+            {BASE_URL + `/${event?._id}`}
+          </Text>
+          <IconButton icon={<Icon as={DocumentDuplicateIcon} w={5} h={5} />} />
+        </Flex>
+      </Box>
+    </Skeleton>
   );
 }
