8fe829e8c0fff099a37721c69243b547a1400e5d
Mohammad Imran <imrancrazy200@gmail.com>
2022-05-07 06:59:34 +0530

    fix: make domain env variable public

--- core/constants/index.js
+++ core/constants/index.js
@@ -1,3 +1,4 @@
-export const BASE_URL = process.env.DOMAIN || "http://localhost:3000";
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_DOMAIN || "http://localhost:3000";
 export const APP_BASE_URL = BASE_URL + "/app";
 export const API_BASE_URL = BASE_URL + "/api/app";
