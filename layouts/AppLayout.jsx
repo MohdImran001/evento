b5f1bf90a27442690ff5874c2d05c352d0e3be8d
Mohammad Imran <imrancrazy200@gmail.com>
2022-05-29 23:01:36 +0530

    feat: add route loading bar in app

--- layouts/AppLayout.jsx
+++ layouts/AppLayout.jsx
@@ -1,5 +1,10 @@
+import Head from "next/head";
+import Router from "next/router";
 import NextLink from "next/link";
+
+import NProgress from "nprogress";
 import { signOut } from "next-auth/react";
+
 import {
   Box,
   Flex,
@@ -17,10 +22,23 @@ import {
   Heading,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon, AddIcon } from "@chakra-ui/icons";
+
 import Profile from "core/elements/User/Profile";
 
-const Links = [{ name: "Dashboard", to: "/app" }];
+Router.onRouteChangeStart = (url) => {
+  NProgress.start();
+  console.log(url);
+};
 
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
+
+const Links = [{ name: "Dashboard", to: "/app" }];
 const NavLink = ({ children, link }) => (
   <Link
     px={2}
@@ -40,6 +58,15 @@ export default function AppLayout({ children }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
       <Box px={4} borderBottomWidth="1px" borderBottomColor="#eee">
         <Flex
           h={16}
